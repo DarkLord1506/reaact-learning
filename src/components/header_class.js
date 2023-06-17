b343c1744570a32666be8e7092c4917f16be1b52
DarkLord <abhi.chatterjee300@gmail.com>
2023-06-17 12:02:17 +0530

    Dynamic Styling

--- src/components/header_class.js
+++ src/components/header_class.js
@@ -53,16 +53,32 @@ class Header2 extends Component{
     state = {
         title: 'The keywords are: ',
         keywords: '',
-        count: 0
+        count: 0,
+        active: 'active'
     }  
+    inputChange = (event) =>{
+        const value = event.target.value === ''? false: event.target.value;
+        this.setState({
+            active:value,
+            keywords: event.target.value
+        })
+    }
+    changeColor(){
+        this.setState({
+            active: this.state.active === 'active' ? 'not-active': 'active'
+        })
+    }
 
 
     render(){
         return (
             <>
-            <header> 
+            {/* <header style={{background:`${this.state.active ? 'red':'blue'}`}}></header> */}
+            <header className={this.state.active}> 
                 <div className="logo" onClick={()=>{console.log('I was clicked')}}>Daily News</div>
                 <input onChange={(e)=>this.inputChange(e)}/>
+
+                <button onClick={()=>this.changeColor()}>Change it</button>
             </header>
             </>
         )
