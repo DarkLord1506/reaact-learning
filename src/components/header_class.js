21eb66af4c1415d742f272115bb4cdc2ef256738
DarkLord <abhi.chatterjee300@gmail.com>
2023-06-17 10:55:23 +0530

    React state

--- src/components/header_class.js
+++ src/components/header_class.js
@@ -3,17 +3,45 @@ import React,{Component} from "react"
 
 
 class Header extends Component{
+
+    state = {
+        title: 'The keywords are: ',
+        keywords: '',
+        count: 0
+    }
     
     inputChange = (event) =>{
-        console.log(event.target.value)
+        this.setState({
+            keywords: event.target.value
+        })
     }
 
+    addOne(){
+        this.setState(
+            {count: this.state.count +1}
+        )
+    }
+    addOneNewWay(){
+        this.setState((state,props)=>({
+            count: state.count + 1
+        }))
+    }
+    
+    
+
+
     render(){
+        console.log(this.state)
         return (
             <>
             <header> 
                 <div className="logo" onClick={()=>{console.log('I was clicked')}}>Logo</div>
                 <input onChange={(e)=>this.inputChange(e)}/>
+                <br/>
+                <div>{this.state.title}</div>
+                <div>{this.state.keywords}</div>
+                <div>{this.state.count}</div>
+                <button onClick={()=>this.addOneNewWay()}>Add One</button>
             </header>
             </>
         )
